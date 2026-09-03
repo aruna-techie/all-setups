954942f9dbb54e86e16cdf82b7eeeec4cc5b87d0
aruna-techie <arunagunda1105@gmail.com>
2026-09-03 15:21:41 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,5 @@
 yum install java-21-amazon-corretto -y
+go to official website of jenkins: jenkins.io, copy redhat os 
 sudo wget -O /etc/yum.repos.d/jenkins.repo     https://pkg.jenkins.io/rpm-stable/jenkins.repo
 yum install jenkins -y
 systemctl start jenkins
